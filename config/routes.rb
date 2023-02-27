7c7a72f25394678fa6bf6e26809afa230cd6394e
edyta pilorz <edytapilorz@gmail.com>
2023-02-27 16:41:26 +0100

    routes corrected

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,19 @@ Rails.application.routes.draw do
 
   get 'users/show'
 
-  get 'personnages/search'
-  get 'personnages/show'
-  get 'personnages/new'
-  post 'personnages/create'
-  patch 'personnages/update'
-  delete 'personnages/destroy'
+  resources :personnages, only: [:search, :show, :new, :create, :update, :destroy] do
+    resources :reservations, only: [:create]
+  end
+  resources :reservations, only: [:destroy]
+end
+  # get 'personnages/search'
+  # get 'personnages/show'
+  # get 'personnages/new'
+  # post 'personnages/create'
+  # patch 'personnages/update'
+  # delete 'personnages/destroy'
 
-  post 'reservations/create'
-  delete 'reservations/destroy'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-end
