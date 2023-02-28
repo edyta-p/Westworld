7024b49210e8f674b519b7a613470e09581f3a9f
Jaouad Karroumi <ixyhoo@outlook.fr>
2023-02-28 11:15:18 +0100

    construit du contolleur reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,27 @@
 class ReservationsController < ApplicationController
+  before_action :set_user, only: [:create, :destroy]
+  before_action :set_reservation, only: [:destroy]
+
   def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user = @user
+    if @reservation.save
+      redirect_to reservation_path(@reservation)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @reservation.destroy
+  end
+
+  private
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+  def reservation_params
+    params.require(:reservation).permit(:date)
   end
 end
