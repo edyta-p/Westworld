e9b46147dd97af8f9aa0da52abcca0e706198b7f
edyta pilorz <edytapilorz@gmail.com>
2023-02-28 15:57:06 +0100

    Personnage controller finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  get '/users/:id', to: "users#show"
-  get '/personnages/search', to: "personnages#search"
+  get '/users/:id', to: "users#show", as: :user_show
 
-  resources :personnages, only: %i[new create show update destroy] do
+  resources :personnages, only: %i[new create show edit update destroy] do
+    collection do
+      get 'search'
+    end
+
     resources :reservations, only: [:create]
   end
   resources :reservations, only: [:destroy]
 end
-  # get 'personnages/search'
-  # get 'personnages/show'
-  # get 'personnages/new'
-  # post 'personnages/create'
-  # patch 'personnages/update'
-  # delete 'personnages/destroy'
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
