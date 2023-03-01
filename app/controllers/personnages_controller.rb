2d8225b1ada4ffb5ecf0e2b257ad0f0611734020
Jaouad Karroumi <ixyhoo@outlook.fr>
2023-03-01 15:12:26 +0100

    Ready to merge

--- app/controllers/personnages_controller.rb
+++ app/controllers/personnages_controller.rb
@@ -54,6 +54,6 @@ class PersonnagesController < ApplicationController
   end
 
   def personnage_params
-    params.require(:personnage).permit(:name, :category, :role, :price)
+    params.require(:personnage).permit(:name, :category, :role, :price, :photo)
   end
 end
