39510d062dab70004b08a03096fbfe07708feb7d
edyta pilorz <edytapilorz@gmail.com>
2023-03-02 13:35:28 +0100

    pictures working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @personnage = Personnage.new
-    @my_own_personnages = current_user.personnages
+    @user = current_user
+    @personnage = Personnage.new(personnage_params)
+    @my_own_personnages = @user.personnages
+  end
+
+  def create
+    @user = current_user
+    @personnage = Personnage.new(personnage_params)
+    @personnage.user = @user
+
+    if @personnage.save!
+      redirect_to personnage_path(@personnage)
+    elsif @personnage.name.valid? == false
+      redirect_to new_personnage_path, notice: "The name is already taken"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
